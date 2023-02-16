cd4f480b3096184f209fab924636b7584bc4338f
Anna Dolmatova <annadolmatova01@gmail.com>
2023-02-16 16:24:26 -0500

    added 2 tests testGuideUrlAndHeader and testChangeMetricOfTheTemperature

--- src/test/java/AnnaDolmatovaTest.java
+++ src/test/java/AnnaDolmatovaTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -40,5 +42,53 @@ public class AnnaDolmatovaTest extends BaseTest {
         String actualResult = h2CityCountryHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testGuideUrlAndHeader() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+        String expectedResultUrl = "https://openweathermap.org/guide";
+
+        getDriver().get(url);
+
+        Thread.sleep(5000);
+
+        WebElement guideElementInMenu =  getDriver().findElement(
+                By.xpath("//a[@href='/guide']"));
+        guideElementInMenu.click();
+
+        Thread.sleep(2000);
+
+        String actualResultUrl = getDriver().getCurrentUrl();
+        String actualResultTitle = getDriver().getTitle();
+
+        Assert.assertEquals(actualResultTitle, expectedResultTitle);
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+
+    }
+
+    @Test
+    public void testChangeMetricOfTheTemperature() throws InterruptedException {
+
+        String url = "https://openweathermap.org/";
+        String expectedResult = "°F";
+
+        getDriver().get(url);
+
+        Thread.sleep(5000);
+
+        WebElement menuImperial = getDriver().findElement(
+                By.xpath("//div[@class='option'][text()='Imperial: °F, mph']"));
+
+        menuImperial.click();
+
+        Thread.sleep(2000);
+
+        WebElement temperatureF = getDriver().findElement(
+                By.xpath("//span[@class='heading']"));
+
+        Assert.assertTrue(temperatureF.getText().contains("°F"));
     }
 }
